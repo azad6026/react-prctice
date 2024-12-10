df1355390acd7857d1d1d3ad2744fe44bff1f0f0
Azadeh Faramarzi <aza.fara99@gmai.com.com>
2024-12-11 10:30:33 +1100

    Added and tested chakra ui

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
 // import CalculatorZustand from "./components/calculator-zustand/CalculatorZustand";
-import Calculator from "./components/calculator/Calculator";
+import Calculator from "./projects/calculator/Calculator";
+import { Button } from "@/components/ui/button";
+import { HStack } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
+      <HStack>
+        <Button color="red">Button 1</Button>
+        <Button>Button 2</Button>
+        <Button>Button 3</Button>
+      </HStack>
       <Calculator />
     </>
   );

a3852cc123eb92ab36ab787b9e96a8cfff79f537
Azadeh Faramarzi <aza.fara99@gmai.com.com>
2024-12-11 14:40:42 +1100

    Make a custom hook for fetching games

--- react-project/src/projects/game-station/components/GamesGrid.tsx
+++ react-project/src/projects/game-station/components/GamesGrid.tsx
@@ -1,26 +1,7 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import baseUrl from "../services/api-client";
-import getApiUrl from "../services/api-client";
+import useGames from "../hooks/useGames";
 
-interface Games {
-  id: number;
-  name: string;
-}
-interface ResultsInterface {
-  count: number;
-  results: Games[];
-}
 const GamesGrid = () => {
-  const [games, setGames] = useState<Games[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    fetch(getApiUrl("games"))
-      .then((response) => response.json())
-      .then((data: ResultsInterface) => setGames(data.results))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { games, error } = useGames();
   return (
     <>
       {error && <p>{error}</p>}
---
+++ react-project/src/projects/game-station/hooks/useGames.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import getApiUrl from "../services/api-client";
+
+interface Games {
+  id: number;
+  name: string;
+}
+interface ResultsInterface {
+  count: number;
+  results: Games[];
+}
+const useGames = () => {
+  const [games, setGames] = useState<Games[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+    fetch(getApiUrl("games"), { signal: controller.signal })
+      .then((response) => response.json())
+      .then((data: ResultsInterface) => setGames(data.results))
+      .catch((err) => {
+        if (err.name === "AbortError") return;
+        setError(err.message);
+      });
+    return () => controller.abort();
+  }, []);
+  return { games, error };
+};
+export default useGames;

711ada754e94c7d41cf8d7c6577adbb8fe855ed5
Azadeh Faramarzi <aza.fara99@gmai.com.com>
2024-12-21 07:14:01 +0330

    Updated api

--- react-project/src/projects/game-station/services/api-client.ts
+++ react-project/src/projects/game-station/services/api-client.ts
@@ -1,4 +1,12 @@
 const apiKey = import.meta.env.VITE_GAMES_API_KEY;
 const baseUrl = `https://api.rawg.io/api`;
-const getApiUrl = (path: string) => `${baseUrl}/${path}?key=${apiKey}`;
+
+// Ensure the key is always included
+const getApiUrl = (path: string) => {
+  const hasQueryParams = path.includes("?");
+  const keyParam = `key=${apiKey}`;
+  const separator = hasQueryParams ? "&" : "?";
+  return `${baseUrl}/${path}${separator}${keyParam}`;
+};
+
 export default getApiUrl;

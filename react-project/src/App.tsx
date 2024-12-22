b9711311e9830e28b8aae902b6a72a03be10a73e
Azadeh Faramarzi <aza.fara99@gmai.com.com>
2024-12-22 07:57:41 +0330

    Updated app

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,16 +1,57 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import Layout from "./projects/game-station/Layout";
-import { Genres } from "./projects/game-station/hooks/useGenres";
+import TodoInput from "./projects/todoapp/componens/TodoInput";
+import TodoList from "./projects/todoapp/componens/TodoList";
+import { TodoItem } from "./projects/todoapp/componens/Todo";
+// import Layout from "./projects/game-station/Layout";
+// import { Genres } from "./projects/game-station/hooks/useGenres";
 
 function App() {
-  const [selectedGenre, setSelectedGenre] = useState<Genres | null>(null);
+  // const [selectedGenre, setSelectedGenre] = useState<Genres | null>(null);
+  const [todos, setTodos] = useState<TodoItem[]>(() => {
+    try {
+      const savedTodos = localStorage.getItem("savedTodos");
+      return savedTodos ? JSON.parse(savedTodos) : [];
+    } catch (error) {
+      return [];
+    }
+  });
+  useEffect(() => {
+    localStorage.setItem("savedTodos", JSON.stringify(todos));
+  }, [todos]);
+  const addTodo = (text: string) => {
+    const newTodo: TodoItem = {
+      id: Date.now(),
+      text,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+  const toggleComplete = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+  const removeTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
   return (
     <>
-      <Layout
+      <div className="todo-app">
+        <TodoList
+          todos={todos}
+          toggleComplete={toggleComplete}
+          removeTodo={removeTodo}
+        />
+        <TodoInput addTodo={addTodo} />
+      </div>
+      {/* <Layout
         onSelectGenre={(genre) => setSelectedGenre(genre)}
-      />
+      /> */}
     </>
   );
 }

e8c4a9b8a95c919d119b307ccd9e2d4e57404bec
Azadeh Faramarzi <aza.fara99@gmai.com.com>
2025-01-08 23:03:54 +0330

    Updated app

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,57 +1,70 @@
 // import { useState } from "react";
-import { useEffect, useState } from "react";
 import "./App.css";
-import TodoInput from "./projects/todoapp/componens/TodoInput";
-import TodoList from "./projects/todoapp/componens/TodoList";
-import { TodoItem } from "./projects/todoapp/componens/Todo";
+import AddMovie from "./projects/movie-app/components/AddMovie";
+import MovieList from "./projects/movie-app/components/MovieList";
+import { MovieProvider } from "./projects/movie-app/context/MovieContext";
 // import Layout from "./projects/game-station/Layout";
 // import { Genres } from "./projects/game-station/hooks/useGenres";
 
 function App() {
   // const [selectedGenre, setSelectedGenre] = useState<Genres | null>(null);
-  const [todos, setTodos] = useState<TodoItem[]>(() => {
-    try {
-      const savedTodos = localStorage.getItem("savedTodos");
-      return savedTodos ? JSON.parse(savedTodos) : [];
-    } catch (error) {
-      return [];
-    }
-  });
-  useEffect(() => {
-    localStorage.setItem("savedTodos", JSON.stringify(todos));
-  }, [todos]);
-  const addTodo = (text: string) => {
-    const newTodo: TodoItem = {
-      id: Date.now(),
-      text,
-      completed: false,
-    };
-    setTodos([...todos, newTodo]);
-  };
-  const toggleComplete = (id: number) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
-  };
-  const removeTodo = (id: number) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
+  // const [todos, setTodos] = useState<TodoItem[]>(() => {
+  // try {
+  //   const savedTodos = localStorage.getItem("savedTodos");
+  //   return savedTodos ? JSON.parse(savedTodos) : [];
+  // } catch (error) {
+  //   return [];
+  // }
+  // });
+  // useEffect(() => {
+  //   localStorage.setItem("savedTodos", JSON.stringify(todos));
+  // }, [todos]);
+  // const addTodo = (text: string) => {
+  //   const newTodo: TodoItem = {
+  //     id: Date.now(),
+  //     text,
+  //     completed: false,
+  //   };
+  // };
+  // setTodos([...todos, newTodo]);
+  // const toggleComplete = (id: number) => {
+  //   setTodos(
+  //     todos.map((todo) =>
+  //       todo.id === id ? { ...todo, completed: !todo.completed } : todo
+  //     )
+  //   );
+  // };
+  // const removeTodo = (id: number) => {
+  //   setTodos(todos.filter((todo) => todo.id !== id));
+  // };
 
   return (
     <>
-      <div className="todo-app">
+      <div className="main">
+        {/* <CurrentLocationWether /> */}
+        {/* <Weather city="tehran" /> */}
+        {/* <CitiesWeather /> */}
+
+        {/* <div className="todo-app">
         <TodoList
-          todos={todos}
-          toggleComplete={toggleComplete}
-          removeTodo={removeTodo}
+        todos={todos}
+        toggleComplete={toggleComplete}
+        removeTodo={removeTodo}
         />
         <TodoInput addTodo={addTodo} />
-      </div>
-      {/* <Layout
+        </div> */}
+        {/* <Layout
         onSelectGenre={(genre) => setSelectedGenre(genre)}
-      /> */}
+        /> */}
+      </div>
+      <MovieProvider>
+        <div className="App">
+          <header className="App-header">
+            <h1>Type the movie name to select from the movie list</h1>{" "}
+            <AddMovie /> <MovieList />
+          </header>
+        </div>
+      </MovieProvider>
     </>
   );
 }

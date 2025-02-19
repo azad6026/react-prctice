384565f48073f81db95efee24cc298927ea81bf7
Azadeh Faramarzi <aza.fara99@gmai.com.com>
2025-02-19 14:14:32 +1100

    Added sperate api client

--- data-grid/src/services/api-client.ts
+++ data-grid/src/services/api-client.ts
@@ -4,9 +4,23 @@ export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "ed8dfcff9fea48fd9c9446c1a5be8932",
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
